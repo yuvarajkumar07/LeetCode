c3e6337b634127ce27b216daf05b7a14165b0d49
yuvarajkumar07 <119675925+yuvarajkumar07@users.noreply.github.com>
2025-04-19 13:23:14 +0530

    [2563] [Time Beats: 100.00%] [Memory Beats: 84.51%] - LeetPush

---
+++ Count-the-Number-of-Fair-Pairs.java
@@ -0,0 +1,17 @@
+class Solution{
+    long countAtLeast(int[] nums, long comp) {
+        long ans = 0; int i = 0, j = nums.length - 1;
+        while (i < j) {
+            if (nums[i] + nums[j] >= comp) {
+                ans += j - i;
+                j--;
+            }
+            else i++;
+        }
+        return ans;
+    }
+    public long countFairPairs(int[] nums, int lower, int upper) {
+        Arrays.sort(nums);
+        return countAtLeast(nums, lower) - countAtLeast(nums, upper+1);
+    }
+}
