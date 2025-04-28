6494a9293f9ce8f0905657ae1532f78ddad91163
yuvarajkumar07 <119675925+yuvarajkumar07@users.noreply.github.com>
2025-04-28 19:00:16 +0530

    [165] [Time Beats: 73.84%] [Memory Beats: 47.32%] - LeetPush

--- Compare-Version-Numbers.java
+++ Compare-Version-Numbers.java
@@ -3,9 +3,9 @@ class Solution {
         String[] v1 = version1.split(\\\\\.\);
         String[] v2 = version2.split(\\\\\.\);
 
-        int length = Math.max(v1.length, v2.length);
+        int len = Math.max(v1.length, v2.length);
 
-        for (int i = 0; i < length; i++) {
+        for (int i = 0; i < len; i++) {
             int num1 = i < v1.length ? Integer.parseInt(v1[i]) : 0;
             int num2 = i < v2.length ? Integer.parseInt(v2[i]) : 0;
 
