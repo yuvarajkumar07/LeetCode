2355c7b9645dbdc51dd25d22d952355a683d151d
yuvarajkumar07 <119675925+yuvarajkumar07@users.noreply.github.com>
2025-05-13 14:47:02 +0530

    [3335] [Time Beats: 89.87%] [Memory Beats: 86.08%] - LeetPush

---
+++ Total-Characters-in-String-After-Transformations-I.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int lengthAfterTransformations(String s, int t) {
+        final int MOD = 1000000007;
+        int n = s.length();
+        int[] count = new int[26];
+        for(int i = 0 ; i < n ;i++){
+            count[s.charAt(i)-'a']++;
+        }
+        // System.out.println(Arrays.toString(count));
+        for(int i = 0 ; i < t ; i++){
+            int z = count['z'-'a'];
+            for(int j = 24 ; j >=0 ; j--){
+                count[j + 1] = count[j];
+            }
+            count[0] = z;
+            count[1] = (count[1] + z) % MOD;
+            // System.out.println(Arrays.toString(count));
+        }
+        int res = 0;
+        for(int x : count){
+          res= (res + x) % MOD;
+        }
+        return res;
+        
+    }
+}
