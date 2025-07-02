fe6ee33787f618e9d9f8f9bf9d138b360170c2be
yuvarajkumar07 <119675925+yuvarajkumar07@users.noreply.github.com>
2025-07-02 20:26:24 +0530

    [73] [Time Beats: 72.29%] [Memory Beats: 63.62%] - LeetPush

--- Set-Matrix-Zeroes.java
+++ Set-Matrix-Zeroes.java
@@ -1,52 +1,22 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-
-        boolean firstRowZero = false;
-        boolean firstColZero = false;
-
-        for (int j = 0; j < n; j++) {
-            if (matrix[0][j] == 0) {
-                firstRowZero = true;
-                break;
+        int n=matrix.length;
+        int m=matrix[0].length;
+        int[] row=new int[n];
+        int[] col=new int[m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]==0){
+                row[i]=1;
+                col[j]=1;
+            }
             }
         }
-
-        for (int i = 0; i < m; i++) {
-            if (matrix[i][0] == 0) {
-                firstColZero = true;
-                break;
+        for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(row[i]==1||col[j]==1){
+                matrix[i][j]=0;
             }
-        }
-
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    matrix[i][0] = 0;
-                    matrix[0][j] = 0;
-                }
-            }
-        }
-
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-
-        if (firstRowZero) {
-            for (int j = 0; j < n; j++) {
-                matrix[0][j] = 0;
-            }
-        }
-
-        if (firstColZero) {
-            for (int i = 0; i < m; i++) {
-                matrix[i][0] = 0;
-            }
-        }
+        }}
     }
 }
